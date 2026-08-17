693246389a58131e7c9a887628f6d5b3291e8908
Wolfgang Hoessl <wolfgangh@users.noreply.github.com>
2026-08-17 14:02:14 +0200

    Add React entry

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const el = document.getElementById("root");
+if (!el) {
+  throw new Error("Wurzelknoten #root fehlt.");
+}
+createRoot(el).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
